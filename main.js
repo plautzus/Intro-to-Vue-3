ac2e9e371c4da18e3e614b8834e477906acfa94e
plautzus <rauleog@gmail.con>
2024-10-31 17:41:29 +0100

    Lesson2_1

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Nice Socks. Warm and fluffy.'
+        }
+    }
+})
+
