e1c2989a99dfd8868f891f15593128fcf0efb69c
plautzus <rauleog@gmail.con>
2024-10-31 17:58:10 +0100

    lesson3_1

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            link: 'https://www.eaglefit.de/emsraum-zubehoer/antirutsch-sportsocken-trainingssocken_1150_2515?ReferrerID=7&msclkid=f381f7957e341b5e4ac990b6c8b680d7&utm_source=bing&utm_medium=cpc&utm_campaign=DE%20%7C%20Shopping&utm_term=4588811785565142&utm_content=Alle%20Produkte'
         }
     }
 })
